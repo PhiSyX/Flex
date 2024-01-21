0777ebd89d97311a34e40b09ee186e0440c2f0f9
Mike 'PhiSyX' S <738010+PhiSyX@users.noreply.github.com>
2024-01-21 02:10:10 +0100

    refactor(flex-web-app): Story `ChannelRoom/Channeluserlist`

--- apps/flex-web-app/sys/channel-room/ChannelRoom.story.vue
+++ apps/flex-web-app/sys/channel-room/ChannelRoom.story.vue
@@ -1,10 +1,52 @@
 <script setup lang="ts">
 import { ChannelUsers } from "~/channel/ChannelUsers";
 import ChannelRoom from "./ChannelRoom.vue";
+import { ChannelNick } from "~/channel/ChannelNick";
+import { ChannelAccessLevel } from "~/channel/ChannelAccessLevel";
+import { RoomMessage } from "~/room/RoomMessage";
 
 const channelName = "#channel";
+
 const messages = [];
+
+const origin1: Origin = {
+	access_level: ["Owner"],
+	id: "uuid0",
+	host: { cloaked: "*" },
+	ident: "ident",
+	nickname: "ModeratorUser",
+};
+
+messages.push(
+	new RoomMessage()
+		.withData({ origin: origin1 })
+		.withID("id")
+		.withIsMe(false)
+		.withMessage("Hello World")
+		.withNickname("ModeratorUser")
+		.withTarget(channelName)
+		.withTime(new Date())
+		.withType("privmsg")
+);
+
 const users = new ChannelUsers();
+
+const origin2: Origin = {
+	id: "uuid1",
+	host: { cloaked: "*" },
+	ident: "ident",
+	nickname: "VipUser",
+};
+const origin3: Origin = {
+	id: "uuid3",
+	host: { cloaked: "*" },
+	ident: "ident",
+	nickname: "User",
+};
+
+users.add(new ChannelNick(origin1).withAccessLevel(ChannelAccessLevel.Owner));
+users.add(new ChannelNick(origin2).withAccessLevel(ChannelAccessLevel.Vip));
+users.add(new ChannelNick(origin3).withAccessLevel(ChannelAccessLevel.User));
 </script>
 
 <template>
--- apps/flex-web-app/sys/channel-userlist/ChannelUserlist.story.vue
+++ apps/flex-web-app/sys/channel-userlist/ChannelUserlist.story.vue
@@ -1,9 +1,34 @@
 <script setup lang="ts">
+import { ChannelNick } from "~/channel/ChannelNick";
 import ChannelUserlist from "./ChannelUserlist.vue";
 import { ChannelUsers } from "~/channel/ChannelUsers";
+import { ChannelAccessLevel } from "~/channel/ChannelAccessLevel";
 
 const channelName = "#channel";
 const users = new ChannelUsers();
+
+const origin1: Origin = {
+	id: "uuid0",
+	host: { cloaked: "*" },
+	ident: "ident",
+	nickname: "ModeratorUser",
+};
+const origin2: Origin = {
+	id: "uuid1",
+	host: { cloaked: "*" },
+	ident: "ident",
+	nickname: "VipUser",
+};
+const origin3: Origin = {
+	id: "uuid3",
+	host: { cloaked: "*" },
+	ident: "ident",
+	nickname: "User",
+};
+
+users.add(new ChannelNick(origin1).withAccessLevel(ChannelAccessLevel.Owner));
+users.add(new ChannelNick(origin2).withAccessLevel(ChannelAccessLevel.Vip));
+users.add(new ChannelNick(origin3).withAccessLevel(ChannelAccessLevel.User));
 </script>
 
 <template>
