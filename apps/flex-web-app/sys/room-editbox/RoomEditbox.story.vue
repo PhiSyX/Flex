2ea1c60985a1443f060949749bc4675acf7701cb
Mike 'PhiSyX' S <738010+PhiSyX@users.noreply.github.com>
2024-01-21 01:55:29 +0100

    feat(flex-web-app): Story `RoomEditbox`

---
+++ apps/flex-web-app/sys/room-editbox/RoomEditbox.story.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import RoomEditbox from "./RoomEditbox.vue";
+</script>
+
+<template>
+	<Story title="Molecules/RoomEditbox" responsive-disabled>
+		<Variant title="Default">
+			<RoomEditbox
+				:disable-input="false"
+				placeholder="Placeholder"
+				target="#channel"
+			/>
+		</Variant>
+
+		<Variant title="Disabled">
+			<RoomEditbox
+				:disable-input="true"
+				placeholder="Placeholder"
+				target="#channel"
+			/>
+		</Variant>
+	</Story>
+</template>
+
+<style>
+#app [data-v-app],
+.histoire-generic-render-story {
+	height: 100%;
+	color: var(--default-text-color);
+}
+</style>
