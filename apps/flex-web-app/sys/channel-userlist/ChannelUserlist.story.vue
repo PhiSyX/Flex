994f54b3fab0273965194410f4add429aea4c69b
Mike 'PhiSyX' S <738010+PhiSyX@users.noreply.github.com>
2024-01-21 01:12:04 +0100

    feat(flex-web-app): Story `ChannelUserlist`

---
+++ apps/flex-web-app/sys/channel-userlist/ChannelUserlist.story.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import ChannelUserlist from "./ChannelUserlist.vue";
+import { ChannelUsers } from "~/channel/ChannelUsers";
+
+const channelName = "#channel";
+const users = new ChannelUsers();
+</script>
+
+<template>
+	<Story title="Molecules/ChannelUserlist" responsive-disabled>
+		<Variant title="Default">
+			<ChannelUserlist :name="channelName" :users="users" />
+		</Variant>
+	</Story>
+</template>
