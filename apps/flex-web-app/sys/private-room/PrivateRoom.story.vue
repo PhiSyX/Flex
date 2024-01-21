f05aac90874504e5cf3db218812b1ec0d883eab5
Mike 'PhiSyX' S <738010+PhiSyX@users.noreply.github.com>
2024-01-21 01:51:15 +0100

    feat(flex-web-app): Story `PrivateRoom`

---
+++ apps/flex-web-app/sys/private-room/PrivateRoom.story.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+import PrivateRoom from "./PrivateRoom.vue";
+import { PrivateNick } from "~/private/PrivateNick";
+
+const origin: Origin = {
+	id: "uuid0",
+	host: { cloaked: "*" },
+	ident: "ident",
+	nickname: "PhiSyX",
+};
+
+const origin1: Origin = {
+	id: "uuid1",
+	host: { cloaked: "*" },
+	ident: "ident",
+	nickname: "User",
+};
+
+const disableInput = false;
+const me: PrivateNick = new PrivateNick(origin);
+const recipient: PrivateNick = new PrivateNick(origin1);
+</script>
+
+<template>
+	<Story title="Molecules/PrivateRoom" responsive-disabled>
+		<Variant title="Me">
+			<PrivateRoom
+				:disable-input="disableInput"
+				:me="me"
+				:recipient="me"
+				:messages="[]"
+			/>
+		</Variant>
+
+		<Variant title="User">
+			<PrivateRoom
+				:disable-input="disableInput"
+				:me="me"
+				:recipient="recipient"
+				:messages="[]"
+			/>
+		</Variant>
+
+		<Variant title="User blocked">
+			<PrivateRoom
+				:disable-input="true"
+				:me="me"
+				:recipient="recipient"
+				:messages="[]"
+			/>
+		</Variant>
+	</Story>
+</template>
+
+<style>
+#app [data-v-app],
+.histoire-generic-render-story {
+	height: 100%;
+	color: var(--default-text-color);
+}
+
+.room\/private {
+	height: 100%;
+}
+</style>
