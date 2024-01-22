ac84da627e819ca397aa71cc6fc3b698279916de
Mike 'PhiSyX' S <738010+PhiSyX@users.noreply.github.com>
2024-01-22 21:45:52 +0100

    feat(web): gestion de la réponse des paramètres de salon

---
+++ apps/flex-web-app/src/modules/mode/socket.d.ts
@@ -0,0 +1,32 @@
+// ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+// ┃ Copyright: (c) 2024, Mike 'PhiSyX' S. (https://github.com/PhiSyX)         ┃
+// ┃ SPDX-License-Identifier: MPL-2.0                                          ┃
+// ┃ ╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌ ┃
+// ┃                                                                           ┃
+// ┃  This Source Code Form is subject to the terms of the Mozilla Public      ┃
+// ┃  License, v. 2.0. If a copy of the MPL was not distributed with this      ┃
+// ┃  file, You can obtain one at https://mozilla.org/MPL/2.0/.                ┃
+// ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+declare interface ModeApplyFlag<F> {
+	flag: F;
+	args: Array<string>;
+	updated_at: string;
+	updated_by: string;
+}
+
+declare interface CommandResponsesFromServer {
+	MODE: {
+		target: string;
+		updated: boolean;
+
+		added: [
+			| ["k", ModeApplyFlag<{ key: string }>]
+			| ["m", ModeApplyFlag<"moderate">]
+			| ["n", ModeApplyFlag<"no_external_messages">]
+			| ["t", ModeApplyFlag<"no_topic">],
+		];
+
+		removed: CommandResponsesFromServer["MODE"]["added"];
+	};
+}
---
+++ apps/flex-web-app/sys/room-events/RoomEventMode.vue
@@ -0,0 +1,89 @@
+<script setup lang="ts">
+import { computed } from "vue";
+
+// ---- //
+// Type //
+// ---- //
+
+interface Props {
+	data: GenericReply<"MODE">;
+	id: string;
+	message: string;
+	isMe: boolean;
+	nickname: string;
+	target: string;
+	time: {
+		datetime: string;
+		formattedTime: string;
+	};
+	type:
+		| "action"
+		| `error:${string}`
+		| "event"
+		| `event:${string}`
+		| "privmsg";
+}
+
+// --------- //
+// Composant //
+// --------- //
+const props = defineProps<Props>();
+
+const addedModesKeys = computed(() => Object.keys(props.data.added));
+const hasAddedModes = computed(() => addedModesKeys.value.length > 0);
+
+const removedModesKeys = computed(() => Object.keys(props.data.removed));
+const hasRemovedModes = computed(() => removedModesKeys.value.length > 0);
+
+const updatedBy = computed(() => {
+	const x = Object.values(props.data.added).at(-1)?.[1].updated_by;
+	const y = Object.values(props.data.removed).at(-1)?.[1].updated_by;
+	return x || y;
+});
+</script>
+
+<template>
+	<time :datetime="time.datetime">
+		{{ time.formattedTime }}
+	</time>
+	<p>
+		<template v-if="!data.updated">* Paramètres du salon: </template>
+		<template v-else>
+			*
+			<span>{{ updatedBy }}</span>
+			a défini les modes:
+		</template>
+
+		<output v-if="hasAddedModes">+</output>
+		<output v-for="[letter, _] in data.added">
+			{{ letter }}
+		</output>
+
+		<output v-if="hasRemovedModes">-</output>
+		<output v-for="[letter, _] of data.removed">
+			{{ letter }}
+		</output>
+		{{ " " }}
+		<bdi v-for="[_, mode] in data.added">
+			{{ mode.args.join(" ") }}{{ " " }}
+		</bdi>
+		{{ " " }}
+		<bdi v-for="[_, mode] in data.removed">
+			{{ mode.args.join(" ") }}{{ " " }}
+		</bdi>
+	</p>
+</template>
+
+<style scoped lang="scss">
+@use "scss:~/flexsheets" as fx;
+
+p {
+	color: var(--color-grey500);
+}
+
+bdi,
+output,
+span {
+	color: var(--default-text-color);
+}
+</style>
