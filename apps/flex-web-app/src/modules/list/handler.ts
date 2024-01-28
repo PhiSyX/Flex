b6da261cef1478400c1f06cff394d2269b1668fc
Mike 'PhiSyX' S <738010+PhiSyX@users.noreply.github.com>
2024-01-28 16:35:01 +0100

    feat(web): prise en charge du module `LIST` (ui)

---
+++ apps/flex-web-app/src/modules/list/handler.ts
@@ -0,0 +1,68 @@
+// ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+// ┃ Copyright: (c) 2024, Mike 'PhiSyX' S. (https://github.com/PhiSyX)         ┃
+// ┃ SPDX-License-Identifier: MPL-2.0                                          ┃
+// ┃ ╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌ ┃
+// ┃                                                                           ┃
+// ┃  This Source Code Form is subject to the terms of the Mozilla Public      ┃
+// ┃  License, v. 2.0. If a copy of the MPL was not distributed with this      ┃
+// ┃  file, You can obtain one at https://mozilla.org/MPL/2.0/.                ┃
+// ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+import { ChatStore } from "~/store/ChatStore";
+
+// -------------- //
+// Implémentation //
+// -------------- //
+
+export class ReplyListHandler implements SocketEventInterface<"RPL_LIST"> {
+	// ----------- //
+	// Constructor //
+	// ----------- //
+	constructor(private store: ChatStore) {}
+
+	// ------- //
+	// Méthode //
+	// ------- //
+
+	listen() {
+		this.store.on("RPL_LIST", (data) => this.handle(data));
+	}
+
+	handle(data: GenericReply<"RPL_LIST">) {
+	}
+}
+
+export class ReplyListstartHandler implements SocketEventInterface<"RPL_LISTSTART"> {
+	// ----------- //
+	// Constructor //
+	// ----------- //
+	constructor(private store: ChatStore) {}
+
+	// ------- //
+	// Méthode //
+	// ------- //
+
+	listen() {
+		this.store.on("RPL_LISTSTART", (data) => this.handle(data));
+	}
+
+	handle(_: GenericReply<"RPL_LISTSTART">) {
+	}
+}
+
+export class ReplyListendHandler implements SocketEventInterface<"RPL_LISTEND"> {
+	// ----------- //
+	// Constructor //
+	// ----------- //
+	constructor(private store: ChatStore) {}
+
+	// ------- //
+	// Méthode //
+	// ------- //
+
+	listen() {
+		this.store.on("RPL_LISTEND", (data) => this.handle(data));
+	}
+
+	handle(_: GenericReply<"RPL_LISTEND">) {}
+}
