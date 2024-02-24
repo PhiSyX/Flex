8b6c7b5974899d5a83c7de9ba82da7095f23d5f3
Mike 'PhiSyX' S. <738010+PhiSyX@users.noreply.github.com>
2024-02-24 18:16:20 +0100

    fixup! refactor(web): module d'invitation

--- www/flex-ws-server/src/chat/features/invite/application.rs
+++ www/flex-ws-server/src/chat/features/invite/application.rs
@@ -8,20 +8,29 @@
 // ┃  file, You can obtain one at https://mozilla.org/MPL/2.0/.                ┃
 // ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
 
-use crate::src::chat::components::{channel, client};
+use std::borrow::Cow;
+
+use flex_chat_channel::{Channel, ChannelAccessControlInterface, ChannelInterface, ChannelsSessionInterface};
+use flex_chat_client::{ClientInterface, ClientSocketInterface, Socket};
+
 use crate::src::ChatApplication;
 
 // --------- //
 // Interface //
 // --------- //
 
-pub trait InviteApplicationInterface
+pub trait InviteApplicationInterface<'a>
+where
+	Self::Channel: 'a,
 {
+	type Channel: ChannelInterface;
+	type ClientSocket<'cs>: ClientSocketInterface;
+
 	/// Ajoute un utilisateur dans la liste des invitations d'un salon.
 	fn add_user_to_invite_channel(
 		&self,
-		channel: channel::ChannelIDRef,
-		user_invite_id: client::ClientID,
+		channel_id: impl Into<Cow<'a, <Self::Channel as ChannelInterface>::RefID<'a>>>,
+		user_invite_id: <<Self::ClientSocket<'_> as ClientSocketInterface>::Client as ClientInterface>::ClientID,
 	);
 }
 
@@ -29,15 +38,19 @@ pub trait InviteApplicationInterface
 // Implémentation // -> Interface
 // -------------- //
 
-impl InviteApplicationInterface for ChatApplication
+impl<'a> InviteApplicationInterface<'a> for ChatApplication
 {
+	type Channel = Channel;
+	type ClientSocket<'cs> = Socket<'cs>;
+
 	fn add_user_to_invite_channel(
 		&self,
-		channel_id: channel::ChannelIDRef,
-		user_invite_id: client::ClientID,
+		channel_id: impl Into<Cow<'a, <Self::Channel as ChannelInterface>::RefID<'a>>>,
+		user_invite_id: <<Self::ClientSocket<'_> as ClientSocketInterface>::Client as ClientInterface>::ClientID,
 	)
 	{
-		let Some(mut channel) = self.channels.get_mut(channel_id) else {
+		let chid: &<Self::Channel as ChannelInterface>::RefID<'a> = &channel_id.into();
+		let Some(mut channel) = self.channels.get_mut(chid) else {
 			return;
 		};
 		channel.add_invite(user_invite_id);
