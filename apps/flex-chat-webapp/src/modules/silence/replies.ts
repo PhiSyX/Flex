08edf5a688f67ae03586bc7d1e7fd71822ad4c35
Mike 'PhiSyX' S <738010+PhiSyX@users.noreply.github.com>
2024-03-04 21:21:21 +0100

    fix(web): doublons des clés vuejs

--- apps/flex-chat-webapp/src/modules/silence/replies.ts
+++ apps/flex-chat-webapp/src/modules/silence/replies.ts
@@ -28,12 +28,23 @@ export class ReplySilenceHandler implements SocketEventInterface<"SILENCE"> {
 		this.store.on("SILENCE", (data) => this.handle(data));
 	}
 
+	// NOTE(phisyx): la propriété `msgid` est redéfini pour éviter d'avoir des
+	// doublons dans les listes pour Vue (à cause de l'attribut `key`) étant
+	// donnée qu'on se base sur le même ID pour afficher deux messages
+	// différents. Cela n'est pas dérangeant de le redéfinir pour cet événement.
 	handle(data: GenericReply<"SILENCE">) {
 		const currentRoom = this.store.roomManager().active();
 
 		if (data.updated) {
 			const message = "Votre liste des utilisateurs ignorés a été mis à jour";
-			currentRoom.addConnectEvent(data, message);
+			currentRoom.addConnectEvent(
+				{
+					...data,
+					// Voir NOTE ci-haut.
+					tags: { ...data.tags, msgid: `${data.tags.msgid}#1` },
+				},
+				message,
+			);
 		}
 
 		for (const user of data.users) {
@@ -48,6 +59,8 @@ export class ReplySilenceHandler implements SocketEventInterface<"SILENCE"> {
 			if (data.updated) {
 				currentRoom.addEvent("event:silence", {
 					...data,
+					// Voir NOTE ci-haut.
+					tags: { ...data.tags, msgid: `${data.tags.msgid}#2` },
 					isCurrentClient: true,
 				});
 			}
