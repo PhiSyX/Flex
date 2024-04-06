868fbef7e384ab6123bcb6cbe4509e752b34d2f5
Mike 'PhiSyX' S. <738010+PhiSyX@users.noreply.github.com>
2024-04-06 21:55:35 +0200

    feat(utils): correspondance de caractères génériques sur les chaînes de caractères

---
+++ libs/cargo/flex-wildcard-matching/tests/asterisk.rs
@@ -0,0 +1,22 @@
+// ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+// ┃ Copyright: (c) 2023, Mike 'PhiSyX' S. (https://github.com/PhiSyX)         ┃
+// ┃ SPDX-License-Identifier: MPL-2.0                                          ┃
+// ┃ ╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌ ┃
+// ┃                                                                           ┃
+// ┃  This Source Code Form is subject to the terms of the Mozilla Public      ┃
+// ┃  License, v. 2.0. If a copy of the MPL was not distributed with this      ┃
+// ┃  file, You can obtain one at https://mozilla.org/MPL/2.0/.                ┃
+// ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+use flex_wildcard_matching::WildcardMatching;
+
+#[test]
+fn test_iswm_asterisk()
+{
+	assert!("test".iswm("t*st"));
+	assert!(!"t*st".iswm("test"));
+
+	assert!("PhiSyX[absent]".iswm("PhiSyX[*]"));
+	assert!("PhiSyX[[[][][]]]".iswm("PhiSyX[*]"));
+	assert!("PhiSyX[occupe]".iswm("PhiSyX[occup*]"));
+}
---
+++ libs/cargo/flex-wildcard-matching/tests/returning_result.rs
@@ -0,0 +1,30 @@
+// ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+// ┃ Copyright: (c) 2023, Mike 'PhiSyX' S. (https://github.com/PhiSyX)         ┃
+// ┃ SPDX-License-Identifier: MPL-2.0                                          ┃
+// ┃ ╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌ ┃
+// ┃                                                                           ┃
+// ┃  This Source Code Form is subject to the terms of the Mozilla Public      ┃
+// ┃  License, v. 2.0. If a copy of the MPL was not distributed with this      ┃
+// ┃  file, You can obtain one at https://mozilla.org/MPL/2.0/.                ┃
+// ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+use flex_wildcard_matching::WildcardMatching;
+
+#[test]
+fn test_returning_result()
+{
+	assert_eq!(
+		"PhiSyX[bot]".wildcard_match("PhiSyX[???]"),
+		Some("PhiSyX[bot]")
+	);
+	assert_eq!(
+		"PhiSyX[xxx]".wildcard_match("PhiSyX[*]"),
+		Some("PhiSyX[xxx]")
+	);
+
+	assert_eq!(
+		"Hello   World".wildcard_match("Hello*World"),
+		Some("Hello   World")
+	);
+	assert_eq!("Hello   World".wildcard_match("Hello?World"), None);
+}
