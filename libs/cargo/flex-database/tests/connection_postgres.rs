c11f91b8f8c1e5dedcf6d344f31a6d578717a235
Mike 'PhiSyX' S. <738010+PhiSyX@users.noreply.github.com>
2024-04-06 21:49:59 +0200

    feat(database): accès aux bases de données via SQLx

---
+++ libs/cargo/flex-database/tests/connection_postgres.rs
@@ -0,0 +1,28 @@
+// ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+// ┃ Copyright: (c) 2023, Mike 'PhiSyX' S. (https://github.com/PhiSyX)         ┃
+// ┃ SPDX-License-Identifier: MPL-2.0                                          ┃
+// ┃ ╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌ ┃
+// ┃                                                                           ┃
+// ┃  This Source Code Form is subject to the terms of the Mozilla Public      ┃
+// ┃  License, v. 2.0. If a copy of the MPL was not distributed with this      ┃
+// ┃  file, You can obtain one at https://mozilla.org/MPL/2.0/.                ┃
+// ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+use flex_database::sgbd::postgres::PostgresSGBD;
+use flex_database::SGBD;
+
+#[tokio::test]
+async fn test_good_connect()
+{
+	let pg = PostgresSGBD::new("postgres://root:root@localhost:5432").await;
+	assert!(pg.is_ok());
+}
+
+#[tokio::test]
+#[should_panic]
+async fn test_fail_connect()
+{
+	PostgresSGBD::new("postgres://user:pass@host:5432")
+		.await
+		.expect("Une connexion Postgres");
+}
