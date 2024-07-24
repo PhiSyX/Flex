74ea60b1aaaa5c716b19d0eab9445c5c47efffec
Mike 'PhiSyX' S. <738010+PhiSyX@users.noreply.github.com>
2024-07-24 13:57:25 +0200

    feat: inputswitch story

---
+++ apps/web/app-vue/sys/flex-uikit/InputSwitch.story.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+import { InputSwitch, InputSwitchV2 } from "@phisyx/flex-vue-uikit";
+import { ref } from "vue";
+
+const model = ref();
+</script>
+
+<template>
+	<Story title="Atoms/InputSwitch" responsive-disabled>
+		<Variant title="Version 1">
+			<InputSwitch
+				v-model="model"
+				labelN="Non"
+				labelY="Oui"
+				name="remember_me"
+			/>
+		</Variant>
+
+		<Variant title="Version 2">
+			<div style="margin: 40px">
+				<InputSwitchV2
+					v-model="model"
+					name="remember_me"
+				/>
+			</div>
+		</Variant>
+	</Story>
+</template>
