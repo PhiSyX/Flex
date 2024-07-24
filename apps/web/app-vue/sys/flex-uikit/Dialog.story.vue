e84f5e00d6ccd763172ed88df8b1ed3985f27a4c
Mike 'PhiSyX' S. <738010+PhiSyX@users.noreply.github.com>
2024-07-24 13:48:11 +0200

    feat: dialog story

---
+++ apps/web/app-vue/sys/flex-uikit/Dialog.story.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+import { Dialog } from "@phisyx/flex-vue-uikit";
+</script>
+
+<template>
+	<Story title="Molecules/Dialog" responsive-disabled>
+		<Variant title="Default">
+			<Dialog>
+				<template #label>Label</template>
+
+				<template #footer>Footer</template>
+
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+				laudantium sed odit ab aperiam fuga quo, asperiores, ut quia
+				incidunt voluptates at maiores minus, magni illum voluptatibus
+				inventore neque quasi?
+			</Dialog>
+		</Variant>
+	</Story>
+</template>
