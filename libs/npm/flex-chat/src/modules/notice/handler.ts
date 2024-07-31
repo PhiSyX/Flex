57fca7283ad2a2441b415169d992f2b1fd29c6f8
Mike 'PhiSyX' S. <738010+PhiSyX@users.noreply.github.com>
2024-07-31 23:58:23 +0200

    feat(web/flex-discussion): ajoute les notices aux activités des salons

--- libs/npm/flex-chat/src/modules/notice/handler.ts
+++ libs/npm/flex-chat/src/modules/notice/handler.ts
@@ -8,7 +8,10 @@
 // ┃  file, You can obtain one at https://mozilla.org/MPL/2.0/.                ┃
 // ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
 
+import { assertChannelRoom, isChannel } from "../../asserts/room";
+import type { ChannelRoom } from "../../channel/room";
 import { NoticeCustomRoom } from "../../custom_room/notice";
+import type { RoomMessage } from "../../room/message";
 import type { ChatStoreInterface } from "../../store";
 
 // -------------- //
@@ -34,9 +37,12 @@ export class NoticeHandler implements SocketEventInterface<"NOTICE"> {
 		const activeRoom = this.store.roomManager().active();
 
 		const payload = { ...data, isCurrentClient };
-		activeRoom.addEvent("event:notice", payload);
+		const message = activeRoom.addEvent("event:notice", payload);
 
-		if (!isCurrentClient) {
+		if (
+			!isCurrentClient ||
+			(isCurrentClient && isChannel(payload.target))
+		) {
 			const noticeRoom = this.store
 				.roomManager()
 				.getOrInsert(NoticeCustomRoom.ID, () => new NoticeCustomRoom());
@@ -45,5 +51,28 @@ export class NoticeHandler implements SocketEventInterface<"NOTICE"> {
 				noticeRoom.addEvent("event:notice", payload);
 			}
 		}
+
+		// NOTE: envoie la notice dans le panel d'activité du salon.
+		if (!isCurrentClient && isChannel(payload.target)) {
+			const maybeChannel = this.store.roomManager().get(payload.target);
+
+			if (maybeChannel.is_some()) {
+				const channel = maybeChannel.unwrap();
+				assertChannelRoom(channel);
+				this.handleChannel(channel, payload, message);
+			}
+		}
+	}
+
+	handleChannel(
+		channel: ChannelRoom,
+		payload: GenericReply<"NOTICE"> & { isCurrentClient: boolean },
+		message: RoomMessage,
+	) {
+		channel.activities.append("notice", {
+			channelID: channel.id(),
+			nickname: payload.origin.nickname,
+			messageID: message.id,
+		});
 	}
 }
