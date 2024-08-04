ac8c3f813bebcca73cc3720c23676fbafc8c9f66
Mike 'PhiSyX' S. <738010+PhiSyX@users.noreply.github.com>
2024-08-04 21:11:48 +0200

    style(libs/flex-date): respect des conventions de nommages

--- libs/npm/flex-date/src/format.ts
+++ libs/npm/flex-date/src/format.ts
@@ -69,60 +69,60 @@ const LONG_MONTHS_LANG_FR = [
  *
  * - `w` -> Jour de la semaine au format numérique. `z` -> Jour de l'année.
  */
-function day_formats(this: Date): DateKeyFormat {
-	const date = this.getDate();
-	const day = this.getDay();
-	const year = this.getFullYear();
+function day_formats(this: Date): DateKeyFormat
+{
+	let date = this.getDate();
+	let day = this.getDay();
+	let year = this.getFullYear();
 
 	return {
 		/**
 		 * '01' à '31'
 		 */
-		d: (): string => (date < 10 ? "0" : "") + date,
+		d: () => (date < 10 ? "0" : "") + date,
 
 		/**
 		 * 'Dim' à 'Sam'
 		 */
-		D: (): string => LONG_DAYS_LANG_FR[day].slice(0, 3),
+		D: () => LONG_DAYS_LANG_FR[day].slice(0, 3),
 
 		/**
 		 * 1 à 31
 		 */
-		j: (): number => date,
+		j: () => date,
 
 		/**
 		 * 'Dimanche' à 'Samedi'
 		 */
-		l: (): string => LONG_DAYS_LANG_FR[day],
+		l: () => LONG_DAYS_LANG_FR[day],
 
 		/**
 		 * 1 (pour Lundi) à 7 (pour Dimanche)
 		 */
-		N: (): number => (day === 0 ? 7 : day),
+		N: () => (day === 0 ? 7 : day),
 
 		/**
 		 * 1 : 'er' pour "le premier"
 		 * 2 : 'd'  pour "le second"
 		 * 3 : 'e'  pour "le troisième"
 		 */
-		S: (): string => {
-			const dm10 = date % 10;
-			const one = dm10 === 1 && date !== 11;
-			const two = dm10 === 2 && date !== 12;
+		S: () =>
+		{
+			let dm10 = date % 10;
+			let one = dm10 === 1 && date !== 11;
+			let two = dm10 === 2 && date !== 12;
 			return one ? "er" : two ? "d" : "e";
 		},
 
 		/**
 		 * 0 (pour dimanche) à 6 (pour samedi)
 		 */
-		w: (): number => day,
+		w: () => day,
 
 		/**
 		 * 0 à 365
 		 */
-		z: (): number => {
-			return Math.ceil((Date.now() - +new Date(year, 0, 1)) / 86400000);
-		},
+		z: () => Math.ceil((Date.now() - +new Date(year, 0, 1)) / 86400000),
 	};
 }
 
@@ -136,24 +136,26 @@ function day_formats(this: Date): DateKeyFormat {
  *
  * @exemple 42 (la 42e semaine de l'année)
  */
-function week_formats(this: Date): DateKeyFormat {
-	const day = this.getDay();
+function week_formats(this: Date): DateKeyFormat
+{
+	let day = this.getDay();
 
 	return {
-		W: (): string | number => {
-			const day_nr = (day + 6) % 7;
+		W: () =>
+		{
+			let day_nr = (day + 6) % 7;
 
-			const target = new Date(this.valueOf());
+			let target = new Date(this.valueOf());
 			target.setDate(target.getDate() - day_nr + 3);
 
-			const first_thursday = target.valueOf();
+			let first_thursday = target.valueOf();
 			target.setMonth(0, 1);
 
 			if (target.getDay() !== 4) {
 				target.setMonth(0, 1 + ((4 - target.getDay() + 7) % 7));
 			}
 
-			const value = 1 + Math.ceil((first_thursday - +target) / 604800000);
+			let value = 1 + Math.ceil((first_thursday - +target) / 604800000);
 
 			return value < 10 ? `0${value}` : value;
 		},
@@ -175,39 +177,42 @@ function week_formats(this: Date): DateKeyFormat {
  *
  * - `t` -> Nombre de jours dans le mois.
  */
-function month_formats(this: Date): DateKeyFormat {
+function month_formats(this: Date): DateKeyFormat
+{
 	let year = this.getFullYear();
-	const month = this.getMonth();
+	let month = this.getMonth();
 
 	return {
 		/**
 		 * 'Janvier' à 'Décembre'
 		 */
-		F: (): string => LONG_MONTHS_LANG_FR[month],
+		F: () => LONG_MONTHS_LANG_FR[month],
 
 		/**
 		 * 01 à 12
 		 */
-		m: (): number => Number((month < 9 ? "0" : "") + (month + 1)),
+		m: () => Number((month < 9 ? "0" : "") + (month + 1)),
 
 		/**
 		 * 'Jan' à 'Dec'
 		 */
-		M: (): string => {
-			const monthName = LONG_MONTHS_LANG_FR[month];
-			const shortMonthEnds = monthName.indexOf("Jui") >= 0 ? 4 : 3;
+		M: () =>
+		{
+			let monthName = LONG_MONTHS_LANG_FR[month];
+			let shortMonthEnds = monthName.indexOf("Jui") >= 0 ? 4 : 3;
 			return monthName.slice(0, shortMonthEnds);
 		},
 
 		/**
 		 * 1 à 12
 		 */
-		n: (): number => month + 1,
+		n: () => month + 1,
 
 		/**
 		 * 28 à 31
 		 */
-		t: (): number => {
+		t: () =>
+		{
 			let next_month = month + 1;
 
 			if (next_month === 12) {
@@ -236,25 +241,26 @@ function month_formats(this: Date): DateKeyFormat {
  *
  * - `y` -> Année sur 2 chiffres.
  */
-function year_formats(this: Date): DateKeyFormat {
-	const year = this.getFullYear();
-	const day = this.getDay();
+function year_formats(this: Date): DateKeyFormat
+{
+	let year = this.getFullYear();
+	let day = this.getDay();
 
 	return {
 		/**
 		 * 1 si bissextile, 0 sinon.
 		 */
-		L: (): number =>
-			~~(year % 400 === 0 || (year % 100 !== 0 && year % 4 === 0)),
+		L: () => ~~(year % 400 === 0 || (year % 100 !== 0 && year % 4 === 0)),
 
-		o: (): number => {
-			const target = new Date(this.valueOf());
+		o: () =>
+		{
+			let target = new Date(this.valueOf());
 			target.setDate(target.getDate() - ((day + 6) % 7) + 3);
 			return target.getFullYear();
 		},
 
-		Y: (): number => year,
-		y: (): number => Number(String(year).substr(2)),
+		Y: () => year,
+		y: () => Number(String(year).slice(2)),
 	};
 }
 
@@ -283,33 +289,33 @@ function year_formats(this: Date): DateKeyFormat {
  *
  * - `u` -> Microsecondes.
  */
-function hour_formats(this: Date): DateKeyFormat {
-	const hours = this.getHours();
-	const minutes = this.getMinutes();
-	const seconds = this.getSeconds();
-	const ms = this.getMilliseconds();
+function hour_formats(this: Date): DateKeyFormat
+{
+	let hours = this.getHours();
+	let minutes = this.getMinutes();
+	let seconds = this.getSeconds();
+	let ms = this.getMilliseconds();
 
 	return {
-		a: (): string => "h",
+		a: () => "h",
 
-		A: (): string => "H",
+		A: () => "H",
 
-		B: (): number =>
-			Math.floor(
-				((((this.getUTCHours() + 1) % 24) +
-					this.getUTCMinutes() / 60 +
-					this.getUTCSeconds() / 3600) *
-					1000) /
-					24,
-			),
-		g: (): number => hours % 12 || 12,
-		G: (): number => hours,
-		h: (): number =>
-			Number(((hours % 12 || 12) < 10 ? "0" : "") + (hours % 12 || 12)),
-		H: (): number => Number((hours < 10 ? "0" : "") + hours),
-		i: (): number => Number((minutes < 10 ? "0" : "") + minutes),
-		s: (): number => Number((seconds < 10 ? "0" : "") + seconds),
-		u: (): number => Number((ms < 10 ? "00" : ms < 100 ? "0" : "") + ms),
+		B: () => Math.floor(
+			(((
+				(this.getUTCHours() + 1) % 24)
+				+ this.getUTCMinutes() / 60
+				+ this.getUTCSeconds() / 3600
+			) * 1000 ) / 24,
+		),
+
+		g: () => hours % 12 || 12,
+		G: () => hours,
+		h: () => Number(((hours % 12 || 12) < 10 ? "0" : "") + (hours % 12 || 12)),
+		H: () => Number((hours < 10 ? "0" : "") + hours),
+		i: () => Number((minutes < 10 ? "0" : "") + minutes),
+		s: () => Number((seconds < 10 ? "0" : "") + seconds),
+		u: () => Number((ms < 10 ? "00" : ms < 100 ? "0" : "") + ms),
 	};
 }
 
@@ -332,43 +338,54 @@ function hour_formats(this: Date): DateKeyFormat {
  * - `Z` ->  Décalage horaire en secondes. Le décalage des zones à l'ouest de la
  *   zone UTC est négative, et à l'est, il est positif.
  */
-function tz_formats(this: Date): DateKeyFormat {
-	const offset = this.getTimezoneOffset();
+function tz_formats(this: Date): DateKeyFormat
+{
+	let offset = this.getTimezoneOffset();
 	return {
-		e: (): string => (/\((.*)\)/.exec(new Date().toString()) || [])[1],
-		I: (): number => {
-			const winter_offset = new Date(0).getTimezoneOffset();
+		e: () => (/\((.*)\)/.exec(new Date().toString()) || [])[1],
+		
+		I: () =>
+		{
+			let winter_offset = new Date(0).getTimezoneOffset();
 			return ~~(offset !== winter_offset);
 		},
-		O: (): string => {
-			const offsetD60 = Math.abs(offset / 60);
-			const offsetM60 = Math.abs(offset % 60);
+		
+		O: () =>
+		{
+			let offsetD60 = Math.abs(offset / 60);
+			let offsetM60 = Math.abs(offset % 60);
 			return (
 				(-offset < 0 ? "-" : "+") +
 				(offsetD60 < 10 ? "0" : "") +
 				Math.floor(offsetD60) +
-				(offsetM60 === 0
-					? "00"
-					: (offsetM60 < 10 ? "0" : "") + offsetM60)
+				(
+					offsetM60 === 0
+						? "00"
+						: (offsetM60 < 10 ? "0" : "") + offsetM60
+				)
 			);
 		},
-		P: (): string => {
-			const offsetD60 = Math.abs(offset / 60);
-			const offsetM60 = Math.abs(offset % 60);
+
+		P: () =>
+		{
+			let offsetD60 = Math.abs(offset / 60);
+			let offsetM60 = Math.abs(offset % 60);
 			return (
 				// biome-ignore lint/style/useTemplate: expression illisible sinon.
 				(-offset < 0 ? "-" : "+") +
 				(offsetD60 < 10 ? "0" : "") +
 				Math.floor(offsetD60) +
 				":" +
-				(offsetM60 === 0
-					? "00"
-					: (offsetM60 < 10 ? "0" : "") + offsetM60)
+				(
+					offsetM60 === 0
+						? "00"
+						: (offsetM60 < 10 ? "0" : "") + offsetM60
+				)
 			);
 		},
-		T: (): string =>
-			this.toTimeString().replace(/^.+ \(?([^)]+)\)?$/, "$1"),
-		Z: (): number => offset * 60,
+
+		T: () => this.toTimeString().replace(/^.+ \(?([^)]+)\)?$/, "$1"),
+		Z: () => offset * 60,
 	};
 }
 
@@ -383,24 +400,27 @@ function tz_formats(this: Date): DateKeyFormat {
  *
  * - `U` -> Secondes depuis l'époque Unix (1er Janvier 1970, 0h00 00s GMT).
  */
-function full_time_formats(this: Date): DateKeyFormat {
+function full_time_formats(this: Date): DateKeyFormat
+{
 	return {
-		c: (): string => format_date("Y-m-d\\TH:i:sP", this),
-		r: (): string => this.toString(),
-		U: (): number => this.getTime() / 1000,
+		c: () => format_date("Y-m-d\\TH:i:sP", this),
+		r: () => this.toString(),
+		U: () => this.getTime() / 1000,
 	};
 }
 
-function zero_padding(str: number | string): string {
-	const $str = String(str);
+function zero_padding(str: number | string): string
+{
+	let $str = String(str);
 	return $str.length === 1 ? `0${$str}` : $str;
 }
 
 /**
  * Converti un objet de type Date en un format passé par le premier argument.
  */
-function format_date(format = "[H:i:s]", time: Date = new Date()): string {
-	const replace_chars = {
+function format_date(format = "[H:i:s]", time: Date = new Date()): string
+{
+	let replace_chars = {
 		...day_formats.apply(time),
 		...week_formats.apply(time),
 		...month_formats.apply(time),
