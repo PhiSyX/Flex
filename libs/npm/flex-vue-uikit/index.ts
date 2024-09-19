67d45b479aaa8e4f5ef11cd9410a3c93756ead21
Mike 'PhiSyX' S. <738010+PhiSyX@users.noreply.github.com>
2024-09-19 15:55:56 +0200

    feat(flex-uikit): composant TextEditable

--- libs/npm/flex-vue-uikit/index.ts
+++ libs/npm/flex-vue-uikit/index.ts
@@ -44,6 +44,7 @@ export { default as InputSwitchV2 } from "./src/inputswitch/InputSwitchV2.vue";
 export { default as Match } from "./src/match/Match.vue";
 export { default as UiMenu } from "./src/menu/Menu.vue";
 export { default as UiMenuItem } from "./src/menu/MenuItem.vue";
+export { default as TextEditable } from "./src/texteditable/TextEditable.vue";
 export { default as TextInput } from "./src/textinput/TextInput.vue";
 
 export { ButtonIcon, ICON_NAMES, LabelIcon } from "./src/icons";
---
+++ libs/npm/flex-vue-uikit/src/texteditable/TextEditable.vue
@@ -0,0 +1,116 @@
+<script setup lang="ts">
+import { nextTick, ref, useTemplateRef } from "vue";
+
+// ---- //
+// Type //
+// ---- //
+
+interface Props {
+	content: string;
+	contentCenter?: boolean;
+	placeholder?: string;
+	placeholderCenter?: boolean;
+	locked: boolean;
+	withLayer?: boolean;
+}
+
+interface Emits {
+	(
+		evt_name: "layer",
+		_: {
+			event: Event;
+			linked_element: HTMLInputElement;
+			mode: boolean;
+		}
+	): void;
+	(evt_name: "submit"): void;
+}
+
+// --------- //
+// Composant //
+// --------- //
+
+const {
+	contentCenter = false,
+	placeholderCenter = true,
+	withLayer = false,
+} = defineProps<Props>();
+const emit = defineEmits<Emits>();
+let input_model = defineModel<string>();
+
+let state = ref(false);
+let $input = useTemplateRef("$input");
+
+// ------- //
+// Handler //
+// ------- //
+
+function submit_handler(evt: Event) {
+	state.value = false;
+	emit("submit");
+
+	if (withLayer) {
+		evt.preventDefault();
+		toggle_layer(evt);
+	}
+}
+
+function open_layer(evt: Event) {
+	state.value = true;
+
+	if (withLayer) {
+		toggle_layer(evt);
+	}
+}
+
+function toggle_layer(evt: Event) {
+	nextTick(() => {
+		$input.value?.focus();
+
+		emit("layer", {
+			event: evt,
+			linked_element: $input.value as HTMLInputElement,
+			mode: state.value,
+		});
+	});
+}
+</script>
+
+<template>
+	<input
+		v-if="state"
+		ref="$input"
+		v-model="input_model"
+		class="[ input:reset size:full ]"
+		:class="{
+			'align-t:center': contentCenter,
+		}"
+		type="text"
+		@blur="submit_handler"
+		@keydown.enter="submit_handler"
+		@keydown.esc="submit_handler"
+	/>
+	<output
+		v-else-if="content.length > 0"
+		class="[ display-ib size:full p=1 select:none ]"
+		:class="{
+			'cursor:default': locked,
+			'cursor:pointer': !locked,
+		}"
+		@dblclick="open_layer"
+	>
+		{{ content }}
+	</output>
+	<p
+		v-else-if="placeholder"
+		class="[ flex h:full my=0 select:none ]"
+		:class="{
+			'cursor:default': locked,
+			'cursor:pointer': !locked,
+			'flex/center:full': placeholderCenter,
+		}"
+		@dblclick="open_layer"
+	>
+		{{ placeholder }}
+	</p>
+</template>
